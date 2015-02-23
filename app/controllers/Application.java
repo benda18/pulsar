523b8824990b7bde5335c9799d119e3458c3b38d
Tim Bender <bendertj@gmail.com>
2015-02-23 11:46:34 -0500

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -27,7 +27,8 @@ import views.html.*;
 
 public class Application extends Controller {
     private static Map<String, TransferExtractor> transferExtractorPool = new HashMap<String, TransferExtractor>();
-    
+    private static Map<String, TransferExtractor3> transferExtractorPool3 = new HashMap<String, TransferExtractor3>();	//$tim33
+	
     // initialize the extractor pool
     static {
         for (File file : new File(Play.application().configuration().getString("gtfs-directory")).listFiles()) {
@@ -37,6 +38,16 @@ public class Application extends Controller {
             }
         }
     }
+
+    // initialize the extractor pool																				//$tim33
+    static {																										//$tim33
+        for (File file : new File(Play.application().configuration().getString("gtfs-directory")).listFiles()) {	//$tim33
+            if (!file.isDirectory() && file.getName().endsWith(".zip")) {											//$tim33
+                String name3 = file.getName().replace(".zip", "");													//$tim33
+                transferExtractorPool3.put(name3, new TransferExtractor3(file));										//$tim33
+            }																										//$tim33
+        }																											//$tim33
+    }																												//$tim33
     
     public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -80,6 +91,45 @@ public class Application extends Controller {
         
         return ok(Json.toJson(ret));
     }
+
+    /** get all possible transfers for a given direction */							//$tim33
+    public static Result transfers3 (String file, String route, int direction3) {		//$tim33
+        if (!transferExtractorPool3.containsKey(file))								//$tim33
+            return notFound("No such GTFS feed");									//$tim33
+																					//$tim33
+        TransferExtractor3 t3 = transferExtractorPool3.get(file);						//$tim33
+																					//$tim33
+        Route r3 = t3.feed.routes.get(route);											//$tim33
+																					//$tim33
+        if (r3 == null)																//$tim33
+            return notFound("no such route");										//$tim33
+																					//$tim33
+        if (direction3 != 0 && direction3 != 1)										//$tim33
+            return badRequest("direction must be 0 or 1");							//$tim33
+																					//$tim33
+        RouteDirection3 rd3 = new RouteDirection3(r3, Direction3.fromGtfs(direction3));	//$tim33
+																					//$tim33
+        // 100 meter threshold for transfers										//$tim33
+        Transfer3[] xfers = t3.getTransfers(rd3, 400);  								//$tim33
+											//400 meters (default) = 1,312 feet		//$tim33
+																					//$tim33
+        List<Transfer3> ret3 = new ArrayList<Transfer3>();  							//$tim33
+																					//$tim33
+        Stop[] fromStops = t3.stopsForRouteDirection(rd3);							//$tim33
+        String destName = fromStops[fromStops.length - 1].stop_name;				//$tim33
+																					//$tim33
+        for (Transfer3 xfer : xfers) {  //$tim$$ should be (Transfer				//$tim33
+            xfer.transferTimes3 = t3.transferTimes3(xfer);								//$tim33
+            if (xfer.transferTimes3.length > 0)										//$tim33
+                ret3.add(xfer);														//$tim33
+																					//$tim33
+            Stop[] stops = t3.stopsForRouteDirection(xfer.toRouteDirection3);			//$tim33
+            xfer.toRouteDirection3.destination = stops[stops.length - 1].stop_name;	//$tim33
+            xfer.fromRouteDirection3.destination = destName;							//$tim33
+        }																			//$tim33
+																					//$tim33
+        return ok(Json.toJson(ret3));												//$tim33
+    }																				//$tim33
     
     /** get all the routes for a given file */
     public static Result routes (String file) {
@@ -90,4 +140,14 @@ public class Application extends Controller {
         
         return ok(Json.toJson(t.feed.routes.values()));
     }
-}
+
+
+    public static Result routes3 (String file) {					//$tim33
+        if (!transferExtractorPool3.containsKey(file))				//$tim33
+            return notFound("No such GTFS feed");					//$tim33
+																	//$tim33
+        TransferExtractor3 t3 = transferExtractorPool3.get(file);	//$tim33
+																	//$tim33
+        return ok(Json.toJson(t3.feed.routes.values()));			//$tim33
+    }																//$tim33
+}																//$tim33
